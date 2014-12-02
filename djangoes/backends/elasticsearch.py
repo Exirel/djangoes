2a71e14cdb1e222ffcb1d77bbb6eeec966e9ec00
Florian Strzelecki <florian.strzelecki@gmail.com>
2014-12-02 23:48:21 +0100

    Replace id by doc_id
    I don't like when a variable or an argument is named "id".

--- djangoes/backends/elasticsearch.py
+++ djangoes/backends/elasticsearch.py
@@ -4,6 +4,7 @@ from . import Base
 
 
 class ConnectionWrapper(Base):
+    """Connection wrapper based on the ElasticSearch official library."""
     transport_class = Transport
 
     def __init__(self, alias, server, indices):
@@ -20,29 +21,29 @@ class ConnectionWrapper(Base):
         """Proxy for attributes and methods to the underlying ES client."""
         return getattr(self._es, attr)
 
-    def ping(self):
-        return self._es.ping()
+    # Query methods 
+    # =============
+    # The underlying client requires index names (or alias names) to perform
+    # queries. The connection wrapper overrides these client methods to
+    # automatically uses the configured names (indices and/or aliases).
 
-    def info(self):
-        return self._es.info()
+    def create(self, doc_type, body, doc_id=None, **kwargs):
+        return self._es.create(self.indices, doc_type, body, doc_id, **kwargs)
 
-    def create(self, doc_type, body, id=None, **kwargs):
-        return self._es.create(self.indices, doc_type, body, id, **kwargs)
+    def index(self, doc_type, body, doc_id=None, **kwargs):
+        return self._es.index(self.indices, doc_type, body, doc_id, **kwargs)
 
-    def index(self, doc_type, body, id=None, **kwargs):
-        return self._es.index(self.indices, doc_type, body, id, **kwargs)
+    def exists(self, doc_id, doc_type='_all', **kwargs):
+        return self._es.exists(self.indices, doc_id, doc_type, **kwargs)
 
-    def exists(self, id, doc_type='_all', **kwargs):
-        return self._es.exists(self.indices, id, doc_type, **kwargs)
+    def get(self, doc_id, doc_type='_all', **kwargs):
+        return self._es.get(self.indices, doc_id, doc_type, **kwargs)
 
-    def get(self, id, doc_type='_all', **kwargs):
-        return self._es.get(self.indices, id, doc_type, **kwargs)
+    def get_source(self, doc_id, doc_type='_all', **kwargs):
+        return self._es.get_source(self.indices, doc_id, doc_type, **kwargs)
 
-    def get_source(self, id, doc_type='_all', **kwargs):
-        return self._es.get_source(self.indices, id, doc_type, **kwargs)
-
-    def update(self, doc_type, id, body=None, **kwargs):
-        return self._es.update(self.indices, doc_type, id, body, **kwargs)
+    def update(self, doc_type, doc_id, body=None, **kwargs):
+        return self._es.update(self.indices, doc_type, doc_id, body, **kwargs)
 
     def search(self, doc_type=None, body=None, **kwargs):
         return self._es.search(self.indices, doc_type, body, **kwargs)
@@ -53,11 +54,11 @@ class ConnectionWrapper(Base):
     def search_template(self, doc_type=None, body=None, **kwargs):
         return self._es.search_template(self.indices, doc_type, body, **kwargs)
 
-    def explain(self, doc_type, id, body=None, **kwargs):
-        return self._es.explain(self.indice, doc_type, id, body, **kwargs)
+    def explain(self, doc_type, doc_id, body=None, **kwargs):
+        return self._es.explain(self.indice, doc_type, doc_id, body, **kwargs)
 
-    def delete(self, doc_type, id, **kwargs):
-        return self._es.delete(self.indices, doc_type, id, **kwargs)
+    def delete(self, doc_type, doc_id, **kwargs):
+        return self._es.delete(self.indices, doc_type, doc_id, **kwargs)
 
     def count(self, index=None, doc_type=None, body=None, **kwargs):
         return self._es.count(self.indices, doc_type, body, **kwargs)
@@ -65,17 +66,17 @@ class ConnectionWrapper(Base):
     def delete_by_query(self, index, doc_type=None, body=None, **kwargs):
         return self._es.delete_by_query(self.indices, doc_type, body, **kwargs)
 
-    def percolate(self, doc_type, id=None, body=None, **kwargs):
-        return self._es.percolate(self.indices, doc_type, id, body, **kwargs)
+    def percolate(self, doc_type, doc_id=None, body=None, **kwargs):
+        return self._es.percolate(self.indices, doc_type, doc_id, body, **kwargs)
 
-    def count_percolate(self, doc_type, id=None, body=None, **kwargs):
-        return self._es.count_percolate(self.indices, doc_type, id, body, **kwargs)
+    def count_percolate(self, doc_type, doc_id=None, body=None, **kwargs):
+        return self._es.count_percolate(self.indices, doc_type, doc_id, body, **kwargs)
 
-    def mlt(self, doc_type, id, body=None, **kwargs):
-        return self._es.mlt(self.indices, doc_type, id, body, **kwargs)
+    def mlt(self, doc_type, doc_id, body=None, **kwargs):
+        return self._es.mlt(self.indices, doc_type, doc_id, body, **kwargs)
 
-    def termvector(self, doc_type, id, body=None, **kwargs):
-        return self._es.termvector(self.indices, doc_type, id, body, **kwargs)
+    def termvector(self, doc_type, doc_id, body=None, **kwargs):
+        return self._es.termvector(self.indices, doc_type, doc_id, body, **kwargs)
 
     def mtermvectors(self, index=None, doc_type=None, body=None, **kwargs):
         return self._es.mtermvectors(self.indices, doc_type, body, **kwargs)
